28f2e20463e026bd1251a84ef4386d51226154ac
Mert <mertbaskurt.1@gmail.com>
2025-03-24 19:02:58 +0300

    pages added

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:ecommerceapp/components/bottom_nav_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+
+
+
+
+class _HomePageState extends State<HomePage> {
+
+
+
+  int _selectedIndex = 0;
+
+
+
+void navigateBottomBar(int index) {
+  setState(() {
+    _selectedIndex = index;
+  });
+}
+
+final List<Widget> _pages = [
+  const ShopPage(),
+
+  const CartPage(),
+]
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index) ,
+      ),
+    );
+  }
+}
